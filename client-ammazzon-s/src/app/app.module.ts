e0e32adca5e48842b6f955c0065f34fd3d2f0857
Emanuele1927 <35915808+Emanuele1927@users.noreply.github.com>
2021-03-26 17:27:51 +0100

    Risolto errore appmodule.ts

--- client-ammazzon-s/src/app/app.module.ts
+++ client-ammazzon-s/src/app/app.module.ts
@@ -3,21 +3,24 @@ import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HomePageComponent } from './home-page/home-page.component';
 import { AnagraficaColoriGalliComponent } from './anagrafica-colori-galli/anagrafica-colori-galli.component';
+import { AnagraficaTagliaComponent } from './anagrafica-taglia/anagrafica-taglia.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomePageComponent,
     AnagraficaColoriGalliComponent,
+    AnagraficaTagliaComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
