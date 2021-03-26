4f4a5ea28acdd097f05e62486183b3c283c22ce5
Giulia2Gargano <giulia2gargano@gmail.com>
2021-03-26 18:16:00 +0100

    Update AnagraficaTaglieController.java

--- ammazzon-server/src/main/java/it/sirfin/ammazzonserver/controller/AnagraficaTaglieController.java
+++ ammazzon-server/src/main/java/it/sirfin/ammazzonserver/controller/AnagraficaTaglieController.java
@@ -5,10 +5,43 @@
  */
 package it.sirfin.ammazzonserver.controller;
 
+import it.sirfin.ammazzonserver.dto.ListaTaglieDto;
+import it.sirfin.ammazzonserver.dto.TagliaDto;
+import it.sirfin.ammazzonserver.service.AnagraficaTaglieService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  *
  * @author Admin
  */
+@RestController
+@CrossOrigin("*")
 public class AnagraficaTaglieController {
-    
+
+    @Autowired
+    AnagraficaTaglieService anagraficaTaglieService;
+
+    @RequestMapping("add-taglia")
+    @ResponseBody
+    public ListaTaglieDto aggiungiTaglia(@RequestBody TagliaDto dto) {
+        return anagraficaTaglieService.aggiungiTaglia(dto.getVarianteTaglia());
+    }
+
+    @RequestMapping("rim-taglia")
+    @ResponseBody
+    public ListaTaglieDto rimuoviTaglia(@RequestBody TagliaDto dto) {
+        return anagraficaTaglieService.rimuoviTaglia(dto.getVarianteTaglia());
+    }
+
+    @RequestMapping("conf-taglia")
+    @ResponseBody
+    public ListaTaglieDto confermaModifica(@RequestBody TagliaDto dto) {
+        return anagraficaTaglieService.conferma(dto.getVarianteTaglia());
+    }
+
 }
