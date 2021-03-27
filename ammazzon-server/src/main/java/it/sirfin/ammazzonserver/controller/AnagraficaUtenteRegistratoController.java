e5eaa433a447adf8093f7f6acb10fddf774eb982
MatteoPalombo99 <78491909+MatteoPalombo99@users.noreply.github.com>
2021-03-27 12:10:21 +0100

    Update AnagraficaUtenteRegistrato

---
+++ ammazzon-server/src/main/java/it/sirfin/ammazzonserver/controller/AnagraficaUtenteRegistratoController.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package it.sirfin.ammazzonserver.controller;
+
+import it.sirfin.ammazzonserver.dto.ListaUtentiRegistratiDto;
+import it.sirfin.ammazzonserver.dto.RicercaUtenteDto;
+import it.sirfin.ammazzonserver.dto.UtenteRegistratoDto;
+import it.sirfin.ammazzonserver.service.AnagraficaUtenteRegistratoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin("*")
+@RestController
+public class AnagraficaUtenteRegistratoController {
+
+    @Autowired
+    AnagraficaUtenteRegistratoService anagraficaUtenteRegistratoService;
+
+    @RequestMapping("/aggiorna-utente")
+    @ResponseBody
+    public ListaUtentiRegistratiDto aggiornaTaglie() {
+        return anagraficaUtenteRegistratoService.aggiorna();
+    }
+
+    @RequestMapping("aggiungi-utente")
+    @ResponseBody
+    public ListaUtentiRegistratiDto aggiungiColore(@RequestBody UtenteRegistratoDto dto) {
+        return anagraficaUtenteRegistratoService.aggiungiUtente(dto.getUtenteRegistrato());
+    }
+
+    @RequestMapping("rimuovi-utente")
+    @ResponseBody
+    public ListaUtentiRegistratiDto rimuoviTaglia(@RequestBody UtenteRegistratoDto dto) {
+        return anagraficaUtenteRegistratoService.rimuoviUtente(dto.getUtenteRegistrato());
+    }
+
+    @RequestMapping("conferma-utente")
+    @ResponseBody
+    public ListaUtentiRegistratiDto confermaTaglia(@RequestBody UtenteRegistratoDto dto) {
+        return anagraficaUtenteRegistratoService.conferma(dto.getUtenteRegistrato());
+    }
+
+    @RequestMapping("rit-utente")
+    @ResponseBody
+    public UtenteRegistratoDto ritornaTaglia(@RequestBody UtenteRegistratoDto dto) {
+        return anagraficaUtenteRegistratoService.ritornaUtente(dto.getUtenteRegistrato());
+    }
+
+    @RequestMapping("ricerca-utente")
+    @ResponseBody
+    public ListaUtentiRegistratiDto confermaTaglia(@RequestBody RicercaUtenteDto dto) {
+        return anagraficaUtenteRegistratoService.ricerca(dto.getCriterioRicerca());
+    }
+
+}
