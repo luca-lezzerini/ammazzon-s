45d8cc3469d09aaff4be068f68c7c888fe0d2d91
nicoloabatecola <78588208+nicoloabatecola@users.noreply.github.com>
2021-03-29 10:14:05 +0200

    UC Associa Taglie a Colori-Prodotti #18

--- ammazzon-server/src/main/java/it/sirfin/ammazzonserver/dto/ListaColoreTagliaDto.java
+++ ammazzon-server/src/main/java/it/sirfin/ammazzonserver/dto/ListaColoreTagliaDto.java
@@ -6,12 +6,14 @@ import java.util.List;
 
 public class ListaColoreTagliaDto {
     private List<ColoreTaglia> coloriTaglie;
+    private List<ColoreTaglia> notColoriTaglie;
 
     public ListaColoreTagliaDto() {
     }
 
-    public ListaColoreTagliaDto(List<ColoreTaglia> coloriTaglie) {
+    public ListaColoreTagliaDto(List<ColoreTaglia> coloriTaglie, List<ColoreTaglia> notColoriTaglie) {
         this.coloriTaglie = coloriTaglie;
+        this.notColoriTaglie = notColoriTaglie;
     }
 
     public List<ColoreTaglia> getColoriTaglie() {
@@ -22,8 +24,18 @@ public class ListaColoreTagliaDto {
         this.coloriTaglie = coloriTaglie;
     }
 
+    public List<ColoreTaglia> getNotColoriTaglie() {
+        return notColoriTaglie;
+    }
+
+    public void setNotColoriTaglie(List<ColoreTaglia> notColoriTaglie) {
+        this.notColoriTaglie = notColoriTaglie;
+    }
+
     @Override
     public String toString() {
-        return "ListaColoreTagliaDto{" + "coloriTaglie=" + coloriTaglie + '}';
+        return "ListaColoreTagliaDto{" + "coloriTaglie=" + coloriTaglie + ", notColoriTaglie=" + notColoriTaglie + '}';
     }
+
+    
 }
---
+++ client-ammazzon-s/src/app/dto/lista-taglia-prodotto-dto.ts
@@ -0,0 +1,6 @@
+import { VarianteTaglia } from "../entità/variante-taglia";
+
+export class ListaTagliaProdottoDto{
+
+    varianteTaglie: VarianteTaglia[] = [];
+}
